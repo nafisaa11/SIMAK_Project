30ee52a51e1b996772d3c4fec640549f9317ad81
nafisaa11 <jnafisah50@gmail.com>
2025-05-25 16:20:15 +0700

    redirect login

--- resources/views/layouts/left-side.blade.php
+++ resources/views/layouts/left-side.blade.php
@@ -142,7 +142,7 @@
             @endhasanyrole
 
             <li>
-                <form method="POST" action="{{ route('login') }}">
+                <form method="POST" action="{{ route('logout') }}">
                     @csrf
                     <button type="submit"
                         class="flex items-center gap-3 w-full text-left px-3 py-2 rounded transition duration-200 hover:bg-gray-800">
